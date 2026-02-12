6e2a79f5c41d6924b42343dc0d3a13369ad82ef1
AndresFPT22 <andresperneth2020@gmail.com>
2026-02-12 18:39:55 -0500

    Create CalcularAreaLongitud.java

---
+++ Actividad1/EjercicioN.°17/CalcularAreaLongitud.java
@@ -0,0 +1,12 @@
+package com.mycompany.ejerciciopropuestonro17;
+
+public class CalcularAreaLongitud {
+    
+    public static double area(double radio){
+        return Math.PI*Math.pow(radio, 2);
+    }
+    
+    public static double longitud(double radio){
+        return 2*Math.PI*radio;
+    }
+}
