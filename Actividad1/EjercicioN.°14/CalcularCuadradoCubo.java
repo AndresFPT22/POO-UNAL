31ce287821f7ed51ee4ecdfdf076332c698dc7dd
AndresFPT22 <andresperneth2020@gmail.com>
2026-02-12 18:38:08 -0500

    CalcularCuadradoCubo class

---
+++ Actividad1/EjercicioN.°14/CalcularCuadradoCubo.java
@@ -0,0 +1,12 @@
+package com.mycompany.ejerciciopropuestonro14;
+
+public class CalcularCuadradoCubo {
+    
+    public static double cuadrado(double number){
+        return Math.pow(number, 2);
+    }
+    
+    public static double cubo(double number){
+        return Math.pow(number, 3);
+    }
+}
