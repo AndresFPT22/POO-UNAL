bb77402dfae95ea744fc3f9bbc9d7f796d41da39
AndresFPT22 <andresperneth2020@gmail.com>
2026-05-26 21:16:49 -0500

    Add Principal class with main method to launch window

---
+++ Actividad3/Ejercicio8.3/Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    
+    public static void main(String[] args) throws Exception {
+       
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+        ventanaPrincipal.setVisible(true);
+    }
+}
