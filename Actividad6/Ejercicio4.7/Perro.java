8bf188977e9dfdae00eb8a41fc7b9450c3e797d9
AndresFPT22 <andresperneth2020@gmail.com>
2026-07-30 23:04:28 -0500

    Add Perro class extending Canido with methods

---
+++ Actividad6/Ejercicio4.7/Perro.java
@@ -0,0 +1,29 @@
+package co.unal.edu;
+
+public class Perro extends Canido {
+
+    public String getNombreCientifico () {
+        
+        return "Canis lupus familiaris";
+        
+    }
+
+    public String getSonido () {
+        
+        return "Ladrido";
+        
+    }
+
+    public String getAlimentos () {
+        
+        return "Carnivora";
+        
+    }
+
+    public String getHabitat () {
+        
+        return "Domestico";
+        
+    }
+    
+}
