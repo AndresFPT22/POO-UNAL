411ed7ea68b794076691deb9b66ec545d07b3a48
AndresFPT22 <andresperneth2020@gmail.com>
2026-07-30 23:05:56 -0500

    Add Gato class extending Felino with properties

---
+++ Actividad6/Ejercicio4.7/Gato.java
@@ -0,0 +1,29 @@
+package co.unal.edu;
+
+public class Gato extends Felino {
+
+    public String getNombreCientifico () {
+        
+        return "Felis silvestris catus";
+        
+    }
+
+    public String getSonido () {
+        
+        return "Maullido";
+        
+    }
+
+    public String getAlimentos () {
+        
+        return "Ratones";
+        
+    }
+
+    public String getHabitat () {
+        
+        return "Domestico";
+        
+    }
+    
+}
