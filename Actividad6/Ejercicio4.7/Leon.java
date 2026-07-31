7a8c3faff2577b12f81e8e0f18e9691ec37579a3
AndresFPT22 <andresperneth2020@gmail.com>
2026-07-30 23:05:23 -0500

    Add Leon class extending Felino with attributes

---
+++ Actividad6/Ejercicio4.7/Leon.java
@@ -0,0 +1,29 @@
+package co.unal.edu;
+
+public class Leon extends Felino {
+
+    public String getNombreCientifico () {
+        
+        return "Panthera leo";
+        
+    }
+
+    public String getSonido () {
+        
+        return "Rugido";
+        
+    }
+
+    public String getAlimentos () {
+        
+        return "Carnivora";
+        
+    }
+
+    public String getHabitat () {
+        
+        return "Pradera";
+        
+    }
+    
+}
