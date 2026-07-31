6704d91edde10594259fc1f0e8aeaa5e9e342e30
AndresFPT22 <andresperneth2020@gmail.com>
2026-07-30 23:04:51 -0500

    Add Lobo class extending Canido with properties

---
+++ Actividad6/Ejercicio4.7/Lobo.java
@@ -0,0 +1,29 @@
+package co.unal.edu;
+
+public class Lobo extends Canido {
+
+    public String getNombreCientifico () {
+        
+        return "Canis lupus";
+        
+    }
+
+    public String getSonido () {
+        
+        return "Aullido";
+        
+    }
+
+    public String getAlimentos () {
+        
+        return "Carnivora";
+        
+    }
+
+    public String getHabitat () {
+        
+        return "Bosque";
+        
+    }
+    
+}
